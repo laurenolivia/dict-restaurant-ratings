088f5d6b1b78bfe40559670163839116ae368007
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 16:28:21 -0700

    trying to create an function that lets the user add restaurant and rating in dictionary

--- ratings.py
+++ ratings.py
@@ -23,5 +23,24 @@ def display_ratings(ratings):
     for restaurant, rating in sorted_ratings:
         print "{} is rated at {}.".format(restaurant, rating)
 
-display_ratings(restaurant_ratings)
+print_ratings = print_ratings(ratings(restaurant_ratings)
 # sorted(ratings.items())
+
+# write function that takes file as arg
+# while loop = True
+# user_input has 3 options, view, add, quit
+# if == "view" run print_display function
+# elif == "add":
+    #add_restaurant_name 
+    #add_rating 1-5 
+
+
+#write function accept two args, restaurant_name, rating 
+def add_to_restaurant_ratings(restaurant_name, rating):
+    restaurant_name = raw_input("Enter the restaurant name: ").capitalize()
+    rating = int(raw_input("Enter rating 1 - 5 only: "))
+
+
+    restaurant_ratings[restaurant_name] = restaurant_ratings.get(rating, 0) + rating
+
+    return restaurant_ratings
