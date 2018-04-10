dbff67b381f7d6b47027b0e5f94f8bc7bd94985d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-09 17:59:19 -0700

    Added add_to_restaurant_ratings and access_ratings functions.

--- ratings.py
+++ ratings.py
@@ -26,21 +26,34 @@ def display_ratings(ratings):
 print_ratings = print_ratings(ratings(restaurant_ratings)
 # sorted(ratings.items())
 
-# write function that takes file as arg
-# while loop = True
-# user_input has 3 options, view, add, quit
-# if == "view" run print_display function
-# elif == "add":
-    #add_restaurant_name 
-    #add_rating 1-5 
-
+def access_ratings(ratings): # write function that takes file as arg
+    """allow user to view ratings or add to ratings"""
+    while True: # while loop = True
+        user_choice = raw_input("Enter [V] to view, [A] to add or [Q] to quit: ").upper() # user_input has 3 options, view, add, quit
+        if user_choice == "Q":
+            break
+        elif user_choice == "V":
+            print_ratings # if == "view" run print_display function
+        elif user_choice == "A": # elif == "add":
+            restaurant_name = raw_input("Enter the restaurant name: ").capitalize()
+            if restaurant_name in restaurant_ratings.keys():
+                print "Restaurant already exists."
+                restaurant_name = raw_input("Enter the restaurant name: ").capitalize()    
+            else:
+                add_to_restaurant_ratings
+    
 
 #write function accept two args, restaurant_name, rating 
-def add_to_restaurant_ratings(restaurant_name, rating):
-    restaurant_name = raw_input("Enter the restaurant name: ").capitalize()
-    rating = int(raw_input("Enter rating 1 - 5 only: "))
+def add_to_restaurant_ratings(restaurant_name):
+    """Add restaurant and rating."""    
+    
+    while True:
+        rating = int(raw_input("Enter rating 1 - 5 only: "))
+        if not rating >= 1 and rating <= 5:
+            print "Rating must be between 1 - 5."
+            continue
+        else:
+            restaurant_ratings[restaurant_name] = rating
+            # restaurant_ratings[restaurant_name] = restaurant_ratings.get(restaurant_name, 0) + rating
 
-
-    restaurant_ratings[restaurant_name] = restaurant_ratings.get(rating, 0) + rating
-
-    return restaurant_ratings
+return restaurant_ratings
